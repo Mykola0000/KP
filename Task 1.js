0a2ad0978fcc5f6b8cd65c42e8384aef6d700e97
Mykola0000 <159796601+Mykola0000@users.noreply.github.com>
2024-05-14 15:55:16 +0200

    Update Task 1.js

--- Task 1.js
+++ Task 1.js
@@ -1,16 +1,22 @@
-
-let array = [];
-for (let i = 0; i < 20; i++) {
-    array.push(Math.floor(Math.random() * 100)); 
-}
-
-
-console.log("Початковий масив:", array);
-
-
-array.sort(function(a, b) {
-    return b - a;
-});
-
-
-console.log("Відсортований масив за спаданням:", array);
+function generateDiv() {
+    let width = document.getElementById("widthSelect").value;
+    let div = document.createElement("div");
+    div.style.backgroundColor = "red";
+    div.style.width = `${width}px`;
+    div.style.height = "30px";
+    document.getElementById("generatedDiv").innerHTML = "";
+    document.getElementById("generatedDiv").appendChild(div);
+  }
+  
+  function moveUp() {
+    let div = document.getElementById("generatedDiv");
+    let currentTop = parseInt(div.style.top) || 0;
+    div.style.top = `${currentTop - 20}px`;
+  }
+  
+  function moveDown() {
+    let div = document.getElementById("generatedDiv");
+    let currentTop = parseInt(div.style.top) || 0;
+    div.style.top = `${currentTop + 20}px`;
+  }
+  
